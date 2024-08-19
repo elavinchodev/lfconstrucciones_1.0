e8336b41ef254e0aed63aba15c760c76a6d882bb
elavincho <elavincho@gmail.com>
2024-08-19 17:50:53 -0300

    Proyecto Terminado

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// Menu Hamburguesa
+
+const nav = document.querySelector("#nav");
+const abrir = document.querySelector("#abrir");
+const cerrar = document.querySelector("#cerrar");
+
+abrir.addEventListener("click", () => {
+    nav.classList.add("visible");
+})
+
+cerrar.addEventListener("click", () => {
+    nav.classList.remove("visible");
+})
+
+// Fin Menu Hamburguesa
